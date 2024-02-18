c5372f1fa54c9787a556a5208cac19049166fba2
Suren1990 <suren.simonyan1990@gmail.com>
2024-02-18 15:23:32 +0400

    added project to git

---
+++ src/router/index.ts
@@ -0,0 +1,50 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import LoginView from '@/views/LoginView.vue'
+import DashboardView from '@/views/DashboardView.vue'
+import NotFoundView from '@/views/NotFoundView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'login',
+      meta: {
+        redirect: 'dashboard',
+      },
+      component: LoginView,
+    },
+    {
+      path: '/dashboard',
+      meta: {
+        isLogined: true,
+      },
+      children: [
+        {
+          path: '',
+          name: 'dashboard',
+          component: DashboardView,
+        },
+      ],
+    },
+    { path: "/:pathMatch(.*)*", name: "NotFound", component: NotFoundView },
+  ]
+});
+
+router.beforeEach((to, from, next) => {
+  const isRouteProtected = to.matched.some(route => route.meta?.isLogined);
+  const hasToken = !!localStorage.getItem("token");
+
+  if (isRouteProtected && !hasToken) {
+    // Если маршрут защищен и нет токена, перенаправляем на страницу входа
+    next({ name: 'login', query: { redirect: to.fullPath } });
+  } else if (to.meta.redirect && hasToken) {
+    // Если есть метаданные для перенаправления и пользователь аутентифицирован
+    next({ name: to.meta.redirect as string });
+  } else {
+    // Во всех остальных случаях продолжаем навигацию
+    next();
+  }
+});
+
+export default router
---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1 class=" text-center font-bold text-8xl">Page not found</h1>
+  </div>
+</template>
